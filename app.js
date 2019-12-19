3d8804501e535eb4440bd6b7e8d6b093346481f5
Marlrus <36910211+Marlrus@users.noreply.github.com>
2019-12-19 11:11:02 -0500

    not getting info from form through

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const   express         = require("express"),
         app             = express(),
         bodyParser      = require("body-parser"),
-        methodOverride  = require("method-override")
+        methodOverride  = require("method-override"),
+        mongoose        = require("mongoose")
     
 require("dotenv").config()
 app.set("view engine","ejs")
 app.use(bodyParser.urlencoded({extended:true}))
+
+mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}).then(() =>{
+    console.log(`Mongoose Connected to: ${mongoose.connection.name}`)
+}).catch(err =>{
+    console.log(`ERROR: ${err.message}`)
+})
 
 app.get("/",(req,res)=>{
     res.render("landing")
@@ -15,9 +26,27 @@ app.get("/about",(req,res)=>{
     res.render("about")
 })
 
+//walks routes CRUD
+//Index
 app.get("/walks",(req,res)=>{
     res.render("walks/index")
 })
+
+//walks new
+app.get("/walks/new",(req,res)=>{
+    res.render("walks/new")
+})
+
+//walks create POST
+app.post("/walks",(req,res)=>{
+    let walk = req.body.walk
+    console.log(walk)
+    res.redirect("/walks")
+})
+
+
+//walks show
+
 
 app.get("/faq",(req,res)=>{
     res.render("faq")
