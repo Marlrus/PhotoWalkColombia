4f54aed9b6f520a01ccc33b2557fc82c094fff52
Marlrus <36910211+Marlrus@users.noreply.github.com>
2019-12-19 10:43:56 -0500

    add basic show templates

--- app.js
+++ app.js
@@ -11,6 +11,21 @@ app.get("/",(req,res)=>{
     res.render("landing")
 })
 
+app.get("/about",(req,res)=>{
+    res.render("about")
+})
+
+app.get("/walks",(req,res)=>{
+    res.render("walks/index")
+})
+
+app.get("/faq",(req,res)=>{
+    res.render("faq")
+})
+app.get("/contact",(req,res)=>{
+    res.render("contact")
+})
+
 const port = process.env.PORT || 3000
 
 app.listen(port, ()=> console.log(`PWC server started in: ${port}`))
